02b632d1b521a175df98f2c4a1da2d880162d124
= <=>
2023-09-28 06:07:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/frontend/FunctionSelectionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.frontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FunctionSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_function_selection)
+    }
+}
